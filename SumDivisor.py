f6f51ed38d171c893d9c51b87b85fc1bd9d77f37
Hyemin <hyemin4246@gmail.com>
2021-04-26 23:30:57 +0900

    20210426 daily commit

---
+++ SumDivisor.py
@@ -0,0 +1,9 @@
+def solution(n):
+    '''정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.'''
+    answer = 0
+    for i in range(1,n+1):
+        if(n%i==0):
+            answer = answer + i
+        else:
+            pass
+    return answer
