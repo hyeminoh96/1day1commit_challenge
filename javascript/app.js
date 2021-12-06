f569a578bfd4813cb44b127efde1ffda854bba7e
hyeminoh <hyemin4246@gmail.com>
2021-12-06 22:25:31 +0900

    obj calculator

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,23 @@
-const player = {
-    name: "Hyemin",
-    age: 25,
+const calculator = {
+    add: function(a, b){
+        console.log("add", a + b);
+    },
+    minus: function(a, b){
+        console.log("minus", a - b);
+    },
+    multi: function(a, b){
+        console.log("multi", a * b);
+    },
+    div: function(a, b){
+        console.log("div", a / b);
+    },
+    power: function(a, b){
+        console.log("power", a ** b);
+    },
 };
 
-player.address = "Seoul";
-
-console.log(player);
+console.log(calculator.add(5, 6));
+console.log(calculator.minus(5, 6))
+console.log(calculator.multi(5, 6))
+console.log(calculator.div(5, 6))
+console.log(calculator.power(5, 6))
