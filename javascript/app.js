0062da6297d51b4c97799ee1d73f6abe90970d96
hyeminoh <hyemin4246@gmail.com>
2021-12-11 17:40:05 +0900

    condition 4

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,13 @@
 const age = parseInt(prompt("How old are you?"));
 
-if(isNaN(age)){
-    console.log("Please write a number");
+if(isNaN(age) || age < 0){
+    console.log("Please write a positive number");
 } else if(age < 18){
     console.log("You are too young");
 } else if (age >= 18 || age <= 50){
     console.log("You can drink");
-} else{
-    console.log("You are too old")
-}s
+} else if (age > 50 && age <= 80){
+    console.log("You should exercise");
+} else if (age > 80){
+    console.log("You can do whatever you want")
+}
