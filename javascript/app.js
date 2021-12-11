97770259ad24c5b7e79e8c6bb44e60280bb8773f
hyeminoh <hyemin4246@gmail.com>
2021-12-11 17:46:18 +0900

    order for conditions

--- javascript/app.js
+++ javascript/app.js
@@ -4,10 +4,12 @@ if(isNaN(age) || age < 0){
     console.log("Please write a positive number");
 } else if(age < 18){
     console.log("You are too young");
-} else if (age >= 18 || age <= 50){
+} else if (age >= 18 && age <= 50){
     console.log("You can drink");
 } else if (age > 50 && age <= 80){
     console.log("You should exercise");
+} else if (age === 100){
+    console.log("WOW! You are wise");
 } else if (age > 80){
     console.log("You can do whatever you want")
-}
+} 
