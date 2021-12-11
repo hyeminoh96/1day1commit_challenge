1c6ef7f56acdc318222317c8ac59923d1918a048
hyeminoh <hyemin4246@gmail.com>
2021-12-11 17:35:29 +0900

    condition 2

--- javascript/app.js
+++ javascript/app.js
@@ -4,6 +4,8 @@ if(isNaN(age)){
     console.log("Please write a number");
 } else if(age < 18){
     console.log("You are too young");
-} else{
+} else if (age >= 18 || age <= 50){
     console.log("You can drink");
-}
+} else{
+    console.log("You are too old")
+}s
