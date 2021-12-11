2496ceba2f94be3460ffed8f115c90a45907dca0
hyeminoh <hyemin4246@gmail.com>
2021-12-11 18:34:46 +0900

    querySelector

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
-const title = document.getElementById("title");
-
-title.innterText = "Got you!";
-
-console.log(title.id);
-console.log(title.className);
+const title = document.getElementsByTagName("h1"); // an array
+console.log(title);
+const title2 = document.querySelector(".hello h1"); // search an element by css selector, return only one
+console.log(title2);
+const title3 = document.querySelectorAll(".hello h1");
+console.log(title3);
