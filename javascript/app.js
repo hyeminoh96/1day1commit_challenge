2d8f8e1d93b55dcc55cbc6385dadf0d45b007a8a
hyeminoh <hyemin4246@gmail.com>
2021-12-11 16:45:31 +0900

    return

--- javascript/app.js
+++ javascript/app.js
@@ -1,23 +1,8 @@
-const calculator = {
-    add: function(a, b){
-        console.log("add", a + b);
-    },
-    minus: function(a, b){
-        console.log("minus", a - b);
-    },
-    multi: function(a, b){
-        console.log("multi", a * b);
-    },
-    div: function(a, b){
-        console.log("div", a / b);
-    },
-    power: function(a, b){
-        console.log("power", a ** b);
-    },
-};
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner + 2;
+}
 
-console.log(calculator.add(5, 6));
-console.log(calculator.minus(5, 6))
-console.log(calculator.multi(5, 6))
-console.log(calculator.div(5, 6))
-console.log(calculator.power(5, 6))
+const krAge = calculateKrAge(age);  
+
+console.log(krAge);
