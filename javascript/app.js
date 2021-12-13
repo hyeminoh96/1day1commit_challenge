81d042a15cb9dcb48a3dda4d76a767f5cec338c3
hyeminoh <hyemin4246@gmail.com>
2021-12-13 22:25:38 +0900

    remove, add

--- javascript/app.js
+++ javascript/app.js
@@ -3,9 +3,9 @@ const h1 = document.querySelector("div.hello:first-child h1");
 function handleTitleClick(){
     const clickedClass = "clicked";
     if(h1.classList.contains(clickedClass)){ // check class list contains clickedclass
-        h1.className = "";  // className can be changed if you want
+        h1.classList.remove(clickedClass);  // className can be changed if you want
     } else {
-        h1.className = clickedClass;
+        h1.classList.add(clickedClass);
     }
 }
 
