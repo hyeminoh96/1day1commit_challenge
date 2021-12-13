6f1eef62f765252def4014ec592b182c5f5704fc
hyeminoh <hyemin4246@gmail.com>
2021-12-13 21:29:30 +0900

    addEventListener

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,8 @@
-const title = document.getElementsByTagName("h1"); // an array
-console.log(title);
-const title2 = document.querySelector(".hello h1"); // search an element by css selector, return only one
-console.log(title2);
-const title3 = document.querySelectorAll(".hello h1");
-console.log(title3);
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+    title.getElementsByClassName.color = "blue";
+
+}
+
+title.addEventListener("click", handleTitleClick);
