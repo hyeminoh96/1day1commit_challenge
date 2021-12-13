591bf29b98e52ee798f75b0dddd9da768d38eb7a
hyeminoh <hyemin4246@gmail.com>
2021-12-13 22:30:54 +0900

    toggle

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,9 @@
 const h1 = document.querySelector("div.hello:first-child h1");
   
 function handleTitleClick(){
-    const clickedClass = "clicked";
-    if(h1.classList.contains(clickedClass)){ // check class list contains clickedclass
-        h1.classList.remove(clickedClass);  // className can be changed if you want
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");  // if classlist contains "clicked", remove "clicked"
+                                    // if classlist doesn't contains "clicked", add "clicked"
+                                    // calling like "clicked" as a token
 }
 
 h1.addEventListener("click", handleTitleClick);
