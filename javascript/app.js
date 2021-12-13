c668bb9c067048c2a46edf2841311d9d296fbed8
hyeminoh <hyemin4246@gmail.com>
2021-12-13 22:05:01 +0900

    style in event

--- javascript/app.js
+++ javascript/app.js
@@ -1,38 +1,15 @@
 const h1 = document.querySelector("div.hello:first-child h1");
+  
 
-function handleTitleClick() {
-    h1.style.color = "blue";
-}      
-
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!")
-}
-
-function handleWindowOffline(){
-    alert("No wifi connected!")
-}
-
-function handleWindowOnline(){
-    alert("Connection So good!")
+function handleTitleClick(){
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("offline", handleWindowOnline);
