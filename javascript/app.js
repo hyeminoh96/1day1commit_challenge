d84f5583cb8bdbfccebc36d08d3f870e0cd8ba8d
hyeminoh <hyemin4246@gmail.com>
2021-12-13 22:23:49 +0900

    contauns

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@ const h1 = document.querySelector("div.hello:first-child h1");
   
 function handleTitleClick(){
     const clickedClass = "clicked";
-    if(h1.className === clickedClass){
+    if(h1.classList.contains(clickedClass)){ // check class list contains clickedclass
         h1.className = "";  // className can be changed if you want
     } else {
         h1.className = clickedClass;
