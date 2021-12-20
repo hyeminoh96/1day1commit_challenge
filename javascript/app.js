8721b786d900d7e8008ec0559b822e2a88789a71
hyeminoh <hyemin4246@gmail.com>
2021-12-20 21:17:24 +0900

    login introduction

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,8 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-  
-function handleTitleClick(){
-    h1.classList.toggle("clicked");  // if classlist contains "clicked", remove "clicked"
-                                    // if classlist doesn't contains "clicked", add "clicked"
-                                    // calling like "clicked" as a token
+const loginInput = document.querySelector("#login-form input");
+const loginInButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+    console.log("hello", loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginInButton.addEventListener("click", onLoginBtnClick);
