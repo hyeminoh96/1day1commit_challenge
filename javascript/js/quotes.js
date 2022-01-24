1608d80b918271b56a77fab17bdf8a18dfca87eb
Hyemin Oh <hyemin4246@gmail.com>
2022-01-24 21:44:22 +0900

    show list randomly

--- javascript/js/quotes.js
+++ javascript/js/quotes.js
@@ -21,4 +21,7 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-console.log(quotes[Math.floor(Math.random() * 10)]);
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author; 
