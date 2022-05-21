26231938b40b091f298af1e7b4e77625e478cee7
Hyemin Oh <hyemin4246@gmail.com>
2022-05-21 13:10:58 +0900

    decorator usage for nested functions

--- python/decorator_function_1.py
+++ python/decorator_function_1.py
@@ -2,21 +2,41 @@ from logging import logger
 
 class ControlledException(Exception):
 
-    def retry(operation):
-        @wraps(operation)
-        def wrapped(*args, **kwargs):
-            last_raised = None
-            RETRIES_LIMIT = 3
-            for _ in range(RETRIES_LIMIT):
-                try:
-                    return operation(*args, **kwargs)
-                except ControlledException as e:
-                    logger.info("retrying %s", operation.__qualname__)
-                    last_raised = e
-            raise last_raised
+    RETRIES_LIMIT = 3
 
-        return wrapped
+    def with_retry(retries_limit=RETRIES_LIMIT, allowed_exceptions=None):
+        allowed_exceptions = allowed_exceptions or (ControlledException,)
 
-    @retry
+        def retry(operation):
+
+            @wraps(operation)
+            def wrapped(*args, **kwargs):
+                last_raised = None    
+                for _ in range(retries_limit):
+                    try:
+                        return operation(*args, **kwargs)
+                    except ControlledException as e:
+                        logger.info("retrying %s due to %s", operation, e)
+                        last_raised = e
+                raise last_raised
+
+            return wrapped
+
+        return retry
+
+    @with_retry()
     def run_operation(task):
         return task.run()
+
+    @with_retry(retries_limit = 5)
+    def run_with_custom_retries_limit(task):
+        return task.run()
+
+    @with_retry(allowed_exceptions=(AttributeError,))
+    def run_with_custom_exceptions(task):
+        return task.run()
+
+    @with_retry(retries_limit=4, allowed_exceptions=(ZeroDivisionError, AttributeError))
+    def run_with_custom_parameters(task):
+        return task.run()
+
